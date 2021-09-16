e3f9f61a48eb03bffad637ee42ec98318acf8171
saashapina <saasha.pina@gmail.com>
2021-09-15 22:39:17 -0400

    add html structure and install scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
-import "./App.css";
+import "./App.scss";
+import { projects, skills } from "./data";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="header">Portfolio</h1>
+      <header>
+        <div className="name">SaaSha Pina</div>
+        <div className="nav">
+          <div>Work</div>
+          <div>About</div>
+          <div>Contact</div>
+        </div>
+      </header>
+
+      <h1>Hi, I'm SaaSha</h1>
+      <h2>Front End Developer and Designer</h2>
+      <p>Building digital products, brands, and experience</p>
+
+      <h2 className="section-header">Work</h2>
+      <div className="projects">
+        {projects.map((project) => (
+          <div className="project" key={project.title}>
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+          </div>
+        ))}
+      </div>
+
+      <h2>About Me</h2>
+      <p>
+        This is all about me Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry. Lorem Ipsum has been the industry's standard
+        dummy text ever since the 1500s, when an unknown printer took a galley
+        of type and scrambled it to make a type specimen book. It has survived
+        not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with
+        the release of Letraset sheets containing Lorem Ipsum passages, and more
+        recently with desktop publishing software like Aldus PageMaker including
+        versions of Lorem Ipsum.
+      </p>
+
+      <h2>Skills</h2>
+      <div className="skills">
+        {skills.map((skill) => (
+          <div className="skill">
+            {skill.title}
+            {skill.description}
+          </div>
+        ))}
+      </div>
+
+      <h2>Contact</h2>
+      <p>contact me</p>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    title: "Project 1",
+    description: "project description",
+  },
+  {
+    title: "Project 2",
+    description: "project description",
+  },
+  {
+    title: "Project 3",
+    description: "project description",
+  },
+  {
+    title: "Project 4",
+    description: "project description",
+  },
+  {
+    title: "Project 5",
+    description: "project description",
+  },
+];
+
+export const skills = [
+  {
+    title: "skill 1",
+    icon: "icon.com/url",
+    description: "about skill here",
+  },
+  {
+    title: "skill 2",
+    icon: "icon.com/url",
+    description: "about skill here",
+  },
+  {
+    title: "skill 3",
+    icon: "icon.com/url",
+    description: "about skill here",
+  },
+  {
+    title: "skill 4",
+    icon: "icon.com/url",
+    description: "about skill here",
+  },
+];
